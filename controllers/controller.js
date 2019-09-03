ab7bc63e386d240d0fe004e8c6be2de29fbd0e7b
Diego F <diego.fernandez.perez@outlook.es>
2019-09-03 09:26:37 +0200

    Entrega ejercicio

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,33 +1,71 @@
 "use strict";
 var car;
-document.getElementById("botonCrearCoche").addEventListener("click", function () {
-    var a = document.getElementById("plate").value;
-    var b = document.getElementById("brand").value;
-    var c = document.getElementById("color").value;
+document
+    .getElementById("botonCrearCoche")
+    .addEventListener("click", function () {
+    var a = document.getElementById("plate")
+        .value;
+    var b = document.getElementById("brand")
+        .value;
+    var c = document.getElementById("color")
+        .value;
     return createCar(a, b, c);
 });
 function createCar(plate, brand, color) {
     car = new Car(plate, color, brand);
-    document.getElementById("carPlate").innerText = "Matricula del coche = " + car.plate;
-    document.getElementById("carBrand").innerText = "Marca del coche = " + car.brand;
-    document.getElementById("carColor").innerText = "Color del coche = " + car.color;
+    if (!car.plate.match(/[0-9]{4}-[a-z]{3}/g)) {
+        car.plate = "Matricula Invalida";
+        document.getElementById("carPlate").innerText =
+            "Matricula del coche = " + car.plate;
+        document.getElementById("carBrand").innerText =
+            "Marca del coche = " + car.brand;
+        document.getElementById("carColor").innerText =
+            "Color del coche = " + car.color;
+    }
+    document.getElementById("carPlate").innerText =
+        "Matricula del coche = " + car.plate;
+    document.getElementById("carBrand").innerText =
+        "Marca del coche = " + car.brand;
+    document.getElementById("carColor").innerText =
+        "Color del coche = " + car.color;
 }
-document.getElementById("botonCrearRuedas").addEventListener("click", function () {
-    var m1 = document.getElementById("brandW1").value;
-    var m2 = document.getElementById("brandW2").value;
-    var m3 = document.getElementById("brandW3").value;
-    var m4 = document.getElementById("brandW4").value;
+document
+    .getElementById("botonCrearRuedas")
+    .addEventListener("click", function () {
+    var m1 = document.getElementById("brandW1")
+        .value;
+    var m2 = document.getElementById("brandW2")
+        .value;
+    var m3 = document.getElementById("brandW3")
+        .value;
+    var m4 = document.getElementById("brandW4")
+        .value;
     var d1 = parseFloat(document.getElementById("diameterW1").value);
     var d2 = parseFloat(document.getElementById("diameterW2").value);
     var d3 = parseFloat(document.getElementById("diameterW3").value);
     var d4 = parseFloat(document.getElementById("diameterW4").value);
-    return createCarRuedas(d1, m1), createCarRuedas(d2, m2), createCarRuedas(d3, m3), createCarRuedas(d4, m4);
+    return (createCarRuedas(d1, m1),
+        createCarRuedas(d2, m2),
+        createCarRuedas(d3, m3),
+        createCarRuedas(d4, m4));
 });
 var contadorRuedas = 1;
 function createCarRuedas(diameter, brand) {
     var wheel1 = new Wheel(diameter, brand);
     car.addWheel(wheel1);
-    document.getElementById("rbrandW" + contadorRuedas).innerHTML = "Marca de la rueda " + contadorRuedas + " es = " + car.wheels[contadorRuedas - 1].brand;
-    document.getElementById("rdiameterW" + contadorRuedas).innerHTML = "Diametro de la rueda " + contadorRuedas + " es = " + car.wheels[contadorRuedas - 1].diameter;
-    contadorRuedas++;
+    console.log(car.wheels[contadorRuedas - 1].diameter < 2);
+    console.log(car.wheels[contadorRuedas - 1].diameter > 0.4);
+    if (car.wheels[contadorRuedas - 1].diameter < 2 && car.wheels[contadorRuedas - 1].diameter > 0.4) {
+        document.getElementById("rbrandW" + contadorRuedas).innerHTML = "Marca de la rueda " + contadorRuedas + " es = " + car.wheels[contadorRuedas - 1].brand;
+        document.getElementById("rdiameterW" + contadorRuedas).innerHTML = "Diametro de la rueda " + contadorRuedas + " es = " + car.wheels[contadorRuedas - 1].diameter;
+        contadorRuedas++;
+    }
+    else {
+        console.log("eso es incorrecto");
+        document.getElementById("rbrandW" + contadorRuedas).innerHTML = "Marca de la rueda " + contadorRuedas + " es = " + car.wheels[contadorRuedas - 1].brand;
+        document.getElementById("rdiameterW" + contadorRuedas).innerHTML = "Diametro de la rueda " + contadorRuedas + " es = Inv\u00E1lido";
+        contadorRuedas++;
+    }
+    ;
 }
+;
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,32 +1,78 @@
-let car:Car;
-document.getElementById("botonCrearCoche").addEventListener("click", function () {
-    let a : string  = (document.getElementById("plate") as HTMLInputElement).value;
-    let b : string  = (document.getElementById("brand") as HTMLInputElement).value;
-    let c : string  = (document.getElementById("color") as HTMLInputElement).value;
-    return createCar(a,b,c)
-});
+let car: Car;
+document
+  .getElementById("botonCrearCoche")
+  .addEventListener("click", function() {
+    let a: string = (document.getElementById("plate") as HTMLInputElement)
+      .value;
+    let b: string = (document.getElementById("brand") as HTMLInputElement)
+      .value;
+    let c: string = (document.getElementById("color") as HTMLInputElement)
+      .value;
+    return createCar(a, b, c);
+  });
 function createCar(plate: string, brand: string, color: string) {
-    car = new Car(plate, color, brand);
-    document.getElementById("carPlate").innerText = "Matricula del coche = " + car.plate;
-    document.getElementById("carBrand").innerText = "Marca del coche = " + car.brand;
-    document.getElementById("carColor").innerText = "Color del coche = " + car.color;
+  car = new Car(plate, color, brand);
+  if (!car.plate.match(/[0-9]{4}-[a-z]{3}/g)) {
+    car.plate = "Matricula Invalida";
+    document.getElementById("carPlate").innerText =
+      "Matricula del coche = " + car.plate;
+    document.getElementById("carBrand").innerText =
+      "Marca del coche = " + car.brand;
+    document.getElementById("carColor").innerText =
+      "Color del coche = " + car.color;
+  }
+  document.getElementById("carPlate").innerText =
+    "Matricula del coche = " + car.plate;
+  document.getElementById("carBrand").innerText =
+    "Marca del coche = " + car.brand;
+  document.getElementById("carColor").innerText =
+    "Color del coche = " + car.color;
 }
-document.getElementById("botonCrearRuedas").addEventListener("click", function () {
-    let m1 : string  = (document.getElementById("brandW1") as HTMLInputElement).value;
-    let m2 : string  = (document.getElementById("brandW2") as HTMLInputElement).value;
-    let m3 : string  = (document.getElementById("brandW3") as HTMLInputElement).value;
-    let m4 : string  = (document.getElementById("brandW4") as HTMLInputElement).value;
-    let d1 : number  = parseFloat((<HTMLInputElement>document.getElementById("diameterW1")).value);
-    let d2 : number  = parseFloat((<HTMLInputElement>document.getElementById("diameterW2")).value);
-    let d3 : number  = parseFloat((<HTMLInputElement>document.getElementById("diameterW3")).value);
-    let d4 : number  = parseFloat((<HTMLInputElement>document.getElementById("diameterW4")).value);
-    return createCarRuedas(d1,m1), createCarRuedas(d2,m2), createCarRuedas(d3,m3), createCarRuedas(d4,m4);
-});
+document
+  .getElementById("botonCrearRuedas")
+  .addEventListener("click", function() {
+    let m1: string = (document.getElementById("brandW1") as HTMLInputElement)
+      .value;
+    let m2: string = (document.getElementById("brandW2") as HTMLInputElement)
+      .value;
+    let m3: string = (document.getElementById("brandW3") as HTMLInputElement)
+      .value;
+    let m4: string = (document.getElementById("brandW4") as HTMLInputElement)
+      .value;
+    let d1: number = parseFloat(
+      (<HTMLInputElement>document.getElementById("diameterW1")).value
+    );
+    let d2: number = parseFloat(
+      (<HTMLInputElement>document.getElementById("diameterW2")).value
+    );
+    let d3: number = parseFloat(
+      (<HTMLInputElement>document.getElementById("diameterW3")).value
+    );
+    let d4: number = parseFloat(
+      (<HTMLInputElement>document.getElementById("diameterW4")).value
+    );
+    return (
+      createCarRuedas(d1, m1),
+      createCarRuedas(d2, m2),
+      createCarRuedas(d3, m3),
+      createCarRuedas(d4, m4)
+    );
+  });
 let contadorRuedas = 1;
 function createCarRuedas(diameter: number, brand: string) {
-    let wheel1 = new Wheel(diameter,brand);
-    car.addWheel(wheel1);
-    document.getElementById("rbrandW"+contadorRuedas).innerHTML = `Marca de la rueda ${contadorRuedas} es = ${car.wheels[contadorRuedas-1].brand}`;
-    document.getElementById("rdiameterW"+contadorRuedas).innerHTML = `Diametro de la rueda ${contadorRuedas} es = ${car.wheels[contadorRuedas-1].diameter}`;
+  let wheel1 = new Wheel(diameter, brand);
+  car.addWheel(wheel1);
+  console.log (car.wheels[contadorRuedas-1].diameter <2);
+  console.log (car.wheels[contadorRuedas-1].diameter >0.4);
+  if (car.wheels[contadorRuedas-1].diameter <2 && car.wheels[contadorRuedas-1].diameter >0.4){
+    document.getElementById("rbrandW" + contadorRuedas).innerHTML = `Marca de la rueda ${contadorRuedas} es = ${car.wheels[contadorRuedas - 1].brand}`;
+    document.getElementById("rdiameterW" + contadorRuedas).innerHTML = `Diametro de la rueda ${contadorRuedas} es = ${car.wheels[contadorRuedas - 1].diameter}`;
     contadorRuedas++;
-}
+  }
+  else {
+    console.log("eso es incorrecto");
+    document.getElementById("rbrandW" + contadorRuedas).innerHTML = `Marca de la rueda ${contadorRuedas} es = ${car.wheels[contadorRuedas - 1].brand}`;
+    document.getElementById("rdiameterW" + contadorRuedas).innerHTML = `Diametro de la rueda ${contadorRuedas} es = Inválido`;
+    contadorRuedas++;
+  };
+};
